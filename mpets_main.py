d969c576d2a05aac07500cbd727a7830d62e7bf3
Wilidon <ildar0364@gmail.com>
2021-01-07 00:25:20 +0400

    Update mpets_main.py

--- mpets_main.py
+++ mpets_main.py
@@ -13,7 +13,7 @@ from config import get_settings, logger_config
 from middlewares import UserMiddleware
 from utils.functions import notice
 
-__version__ = "1.1.5"
+__version__ = "1.1.6"
 
 logging.basicConfig(filename="logs/vk.log",
                     filemode='a',
