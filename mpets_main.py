6b97b7bbcafc126c730ff251955afd32677b1ab5
Ildar <wilidon@bk.ru>
2021-02-13 19:56:10 +0400

    Добавлено новое сердечко.

--- mpets_main.py
+++ mpets_main.py
@@ -13,7 +13,7 @@ from blueprints.user_tasks import user_router
 from config import get_settings, logger_config
 from middlewares import UserMiddleware
 
-__version__ = "1.6.3"
+__version__ = "1.6.4"
 
 logging.basicConfig(filename="logs/vk.log",
                     filemode='a',
--- utils/tasks.py
+++ utils/tasks.py
@@ -835,7 +835,7 @@ async def checking_anketa_htask(mpets, user, user_task):
     task_name = user_task.task_name
     anketa_about = task_name.split("_", maxsplit=1)[-1]
     anketa_about = anketa_about.rsplit(":", maxsplit=1)[0]
-    if profile["about"] in ["❤", "❤️"]:
+    if profile["about"] in ["❤", "❤️", "♥️"]:
         ank = task_name.split("_", maxsplit=1)[-1]
         start_time = ank.rsplit(":", maxsplit=1)[1]
         if int(start_time) == 0:
