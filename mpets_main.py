fe50f14d5351bc34d2cec9b9bc33d286cb33cba0
Ildar <ildar0364@gmail.com>
2021-02-16 11:53:27 +0400

    Лерочка, [16.02.21 11:44] Фишки клубов — шарики, Баллы игроков — медали
    Ильдар, [16.02.21 11:44]
    давай
    
    Лерочка, [16.02.21 11:45]
    🎈
    🏅

--- mpets_main.py
+++ mpets_main.py
@@ -13,7 +13,7 @@ from blueprints.user_tasks import user_router
 from config import get_settings, logger_config
 from middlewares import UserMiddleware
 
-__version__ = "1.6.9"
+__version__ = "1.6.10"
 
 logging.basicConfig(filename="logs/vk.log",
                     filemode='a',
