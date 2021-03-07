e7efd04860e7fc264260ecce335dab4c3c9b9adb
Ildar <ildar0364@gmail.com>
2021-03-07 21:45:37 +0400

    version: 2.3.13

--- keyboards/kb.py
+++ keyboards/kb.py
@@ -29,8 +29,8 @@ async def get_kb(shop: bool = False, access: int = 0, today: int = False, boss_b
                              payload={"command": "0308"},
                              color=ButtonColor.POSITIVE)
         MENU.add_row()
-    today = int(datetime.datetime.today().strftime("%Y%m%d"))
-    if boss_start <= today <= boss_end and (access >= 3 or today != 20210307):
+    today_date = int(datetime.datetime.today().strftime("%Y%m%d"))
+    if boss_start <= today <= boss_end and (access >= 3 or today_date > 20210307):
         if boss_btn is True:
             MENU.add_text_button(text="🦠 Босс",
                                  payload={"command": "boss"},
