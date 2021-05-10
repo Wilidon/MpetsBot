cb14e24a47438398c3b4be46aa61ddf5f4cfdbe3
Ildar <ildar0364@gmail.com>
2021-05-10 23:55:42 +0400

    fix

--- utils/currency.py
+++ utils/currency.py
@@ -311,14 +311,14 @@ async def parce_angel(pet_id, thread_ids, mpets):
 
 
 async def get_currency(user, event):
-    thread_ids = {"popcorn": 2557447,
-                  "plus": 2572662,
-                  "silver": 2573189,
-                  "feather": 2603855,
-                  "key": 2570823,
-                  "angel": [2501851, 2501843, 2501844, 2501845, 2501846, 2501849,
-                            2501856, 2501855, 2501854, 2501853, 2501852, 2531821],
-                  "gear": [2531790]}
+    thread_ids = {"popcorn": 2557447,}
+                  #"plus": 2572662,
+                  #"silver": 2573189,
+                  #"feather": 2603855,
+                  #"key": 2570823,
+                  #"angel": [2501851, 2501843, 2501844, 2501845, 2501846, 2501849,
+                  #          2501856, 2501855, 2501854, 2501853, 2501852, 2531821],
+                  #"gear": [2531790]}
     pet_id = user.pet_id
     name = user.name
     club_id = user.club_id
