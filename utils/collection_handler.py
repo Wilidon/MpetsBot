edccef6a8698c2713281830f91d082240f382926
Ildar <ildar0364@gmail.com>
2021-06-16 23:31:57 +0400

    fix collection type 1

--- utils/collection_handler.py
+++ utils/collection_handler.py
@@ -83,7 +83,7 @@ async def create_collection_item(user_id: int):
     random_part = random.randint(0, 19)
     part_id, collection_id = 1, 0
     if 0 <= random_part <= 4:
-        part_id = random_part
+        part_id = random_part + 1
         collection_id = 1
     elif 5 <= random_part <= 9:
         part_id = random_part - 5 + 1
